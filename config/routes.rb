e03382645442acb113d6dcb05acc69cb1fe87325
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-16 14:38:12 +0000

    Removes original form from homepage. Creates new user route. Adds sign up form at localhost:3000/signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index' # this is our homepage
 
+  get 'signup', to: 'users#new' # get request to localhost:3000/users/new to sign up
+
   resources :posts # an thing in db
+
+  resources :users, except: [:new]
 end
