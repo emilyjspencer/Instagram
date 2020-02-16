085deebc79f0c4a19485b99162c200795dd72459
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-16 13:57:22 +0000

    Adds has_secure_password to User model. Installs bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
  has_many :posts
+ has_secure_password
  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 20 }
  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
  validates :email, presence: true, length: { maximum: 40 }, uniqueness: { case_sensitive: false }, 
