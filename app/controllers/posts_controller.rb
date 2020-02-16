8400739bda9c630f3d0b94f8580ee27c7d8f6df9
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-16 00:57:20 +0000

    User can enter a caption and comments. Button click currently doesn't send the entered data anywhere

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
   def new # action 
+    @post = Post.new
+  end
+
+  def create # crud method - create a new post
+    @post = Post.new(post_params)
+    @post.save # save to db
+  end
+
+  private
+    
+  def post_params
+    params.require(:post).permit(:caption, :comments)
   end
 
 end 
