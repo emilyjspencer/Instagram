68cec89cc287164db58a72458adfa632327e3fde
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-16 19:15:18 +0000

    Creates Sessions controller and view. Adds routes to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index' # this is our homepage
+    get 'signup', to: 'users#new' # get request to localhost:3000/users/new to sign up
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    get 'login', to: 'sessions#new'
 
-  get 'signup', to: 'users#new' # get request to localhost:3000/users/new to sign up
-
-  resources :posts # an thing in db
-
+  resources :posts 
   resources :users, except: [:new]
+
+  #resources :users, except: [:login]
 end
