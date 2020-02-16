f44c534d62bc8749eab82b45fc113c3dc69d8ff2
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-16 12:44:47 +0000

    Creates users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_003602) do
+ActiveRecord::Schema.define(version: 2020_02_16_124319) do
 
   create_table "posts", force: :cascade do |t|
     t.string "caption"
     t.text "comments"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
